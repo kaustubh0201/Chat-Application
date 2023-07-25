4f3e6941af1979ec0d75bd725a4e451b751dda89
kaustubh0201 <kaustubh1818@gmail.com>
2023-07-25 20:07:17 +0530

    added chat server

---
+++ chatserver/src/main/java/com/project/chatserver/model/Message.java
@@ -0,0 +1,19 @@
+package com.project.chatserver.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+
+}
---
+++ chatserver/src/main/java/com/project/chatserver/model/Status.java
@@ -0,0 +1,8 @@
+package com.project.chatserver.model;
+
+public enum Status {
+
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
